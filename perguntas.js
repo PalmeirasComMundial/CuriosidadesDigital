ccd40148465d4fc45d85e742550d0e27a1258979
Richard Emanuel Rodrigues de Matos 3D <00001060681985sp@al.educacao.sp.gov.br>
2024-11-18 20:15:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidade sobre Filmes',
+    'Qual o filme com a maior bilheteria ja feita?',
+    'O filme que mais fez dinheiro no mundo é Avatar (2009), com uma bilheteria mundial de US$ 2,9 bilhões.'
+)
+
+criaCartao(
+    'Curiosidade sobre Jogos',
+    'Qual o jogo é o maior em questão de ocupação de armazenamento?',
+    'O jogo que ocupa mais armazenamento no mundo é Ark: Survival Evolved, que requer 400 GB de espaço de armazenamento para instalação.'
+)
+
+criaCartao(
+    'Curiosidade sobre Smartphones',
+    'Qual celular mais vendido do mundo em 2024?',
+    'Os smartphones mais vendidos do mundo em 2024 pertencem à Apple. Os três primeiros lugares foram ocupados pelo iPhone 15 Pro Max, iPhone 15 e iPhone 15 Pro, respectivamente.'
+)
+
+criaCartao(
+    'Curiosidade sobre Streamings',
+    'Quais foram as plataformas de filmes e series mais usadas em 2024?',
+    'As plataformas de filmes e séries mais usadas em 2024 são Netflix, HBO Max, Disney+ e Amazon Prime Video.'
+)
